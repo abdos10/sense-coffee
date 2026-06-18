7eb9dccb26affcd518aabb49db00798c9a72920e
abdulrazzak <abdulrazzaksabbagh10@gmail.com>
2026-06-18 14:21:18 +0300

    ...

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,12 +1,12 @@
 export const site = {
   name: "Sense Coffee",
   phone: "+974 XXXX XXXX",
-  whatsappNumber: "974XXXXXXXX",
+  whatsappNumber: "97431075711",
   whatsappMessage: "Hello, I would like to inquire about your products",
   foundedYear: 2024,
   location: "Doha, Qatar",
   url: "https://sensecoffee.qa",
   email: "info@sensecoffee.qa",
-  instagram: "https://instagram.com/sensecoffee",
-  facebook: "https://facebook.com/sensecoffee",
+  instagram: "https://www.instagram.com/sense.coffee_qa/",
+  facebook: "https://www.facebook.com/profile.php?id=61572953280800",
 };
